4706eeb13c095d3c39897cb328e5f42a0802f94f
Luis Roberto Perez R <luis.robertop87@gmail.com>
2018-03-27 21:09:16 -0400

    Primera solucion

--- fizzbuzz/src/main/java/edu/uagrm/main/Main.java
+++ fizzbuzz/src/main/java/edu/uagrm/main/Main.java
@@ -5,4 +5,24 @@ public class Main {
     public boolean init() {
         return true;
     }
+
+    public String getFizzBuzz(int Val){
+        if(Val==0){
+            return  String.valueOf(Val);
+        }
+        String result = "";
+        if (Val % 3 == 0){
+            result += "Fizz";
+        }
+        if (Val % 5 == 0){
+            result += "Buzz";
+        }
+        if (result == ""){
+            result = String.valueOf(Val);
+        }
+
+        return  result;
+
+
+    }
 }
--- fizzbuzz/src/test/java/edu/uagrm/main/MainTest.java
+++ fizzbuzz/src/test/java/edu/uagrm/main/MainTest.java
@@ -1,5 +1,6 @@
 package edu.uagrm.main;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,4 +12,47 @@ public class MainTest {
         Main main = new Main();
         assertTrue(main.init());
     }
+
+    @Test
+    public void testGetFizzBuzzIfNumberIsDivisibleByThreeReturnFizz(){
+        Main main = new Main();
+        String expectedValue = "Fizz";
+        String result = main.getFizzBuzz(3);
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void testGetFizzBuzzIfNumberIsDivisibleByFiveReturnBuzz(){
+        Main main = new Main();
+        String expectedValue = "Buzz";
+        String result = main.getFizzBuzz(5);
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void testGetFizzBuzzIfNumberIsDivisibleByFiveAndThreeReturnFizzBuzz(){
+        Main main = new Main();
+        String expectedValue = "FizzBuzz";
+        String result = main.getFizzBuzz(15);
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void testGetFizzBuzzIfNumberIsNoTDivisibleByFiveAndThreeReturnSameNumber(){
+        Main main = new Main();
+        String expectedValue = "1";
+        String result = main.getFizzBuzz(1);
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void testGetFizzBuzzIfNumberIsZeroReturnZero(){
+        Main main = new Main();
+        String expectedValue = "0";
+        String result = main.getFizzBuzz(0);
+        assertEquals(expectedValue, result);
+    }
+
+
+
 }
