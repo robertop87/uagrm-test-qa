1d7907acda44af7760cdaaba5443d95ca2cb473a
Luis Roberto Perez Rios <luis.robertop87@gmail.com>
2018-03-29 23:28:42 -0400

    Refactor to use return as base of algorithm

--- fizzbuzz/src/main/java/edu/uagrm/main/FizzBuzz.java
+++ fizzbuzz/src/main/java/edu/uagrm/main/FizzBuzz.java
@@ -2,25 +2,11 @@ package edu.uagrm.main;
 
 public class FizzBuzz {
 
-    public String eval(int value){
-        if (value == 0) {
-            return String.valueOf(value);
-        }
-
-        String result = "";
-
-        if (value % 3 == 0) {
-            result += "Fizz";
-        }
-
-        if (value % 5 == 0) {
-            result += "Buzz";
-        }
-
-        if (result == "") {
-            result = String.valueOf(value);
-        }
-
-        return  result;
+    public String eval(int value) {
+        if (value == 0) return "0";
+        if ((value % 3) + (value % 5) == 0) return "FizzBuzz";
+        if (value % 3 == 0) return "Fizz";
+        if (value % 5 == 0) return "Buzz";
+        return Integer.toString(value);
     }
 }
